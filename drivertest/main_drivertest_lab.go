ee6fac5eaefa12e816abacdbd40ff260992e3577
root <root@itk-d651>
2015-03-03 20:04:51 +0100

    Added drivertest directory: An environment for developing driver.go with high abstraction

---
+++ drivertest/main_drivertest_lab.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"./driver"
+	"fmt"
+)
+
+const floorNumbers = 3
+const buttonType = 2
+
+func main() {
+	if driver.Init() == 1 {
+		fmt.Println("Driver intialized")
+	}
+
+
+
+
+
+
+
+/*
+	floor := 0
+	driver.SetButtonLamp(1, 1, 0)
+	driver.SetButtonLamp(2, 1, 1)
+
+	for {
+		if driver.GetButtonSignal(0, 0) == 1 {
+			driver.SetButtonLamp(0, 0, 1)
+			driver.SetButtonLamp(2, 0, 1)
+		}
+		if driver.GetFloorSensor() == 2 {
+			if floor != driver.GetFloorSensor() {
+				fmt.Println("Arrived at floor 2")
+				floor = driver.GetFloorSensor()
+			}
+		} else if driver.GetFloorSensor() == 3 {
+		}
+
+	}
+
+*/
+}
