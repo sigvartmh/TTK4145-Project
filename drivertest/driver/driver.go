006760c30a081562727f15ae1a3f729449975862
ATgeir <kurt.kristian.lyngholm@gmail.com>
2015-03-16 19:12:03 +0100

    fiklet litt med driveren

--- drivertest/driver/driver.go
+++ drivertest/driver/driver.go
@@ -1,85 +1,86 @@
 package driver
 
 /*
-#cgo CFLAGS: -std=c99 
-#cgo LDFLAGS: /root/kurtkl/go/src/github.com/sigvartmh/TTK4145-project/drivertest/simulation_elevator.a /root/kurtkl/go/src/github.com/sigvartmh/TTK4145-project/drivertest/libphobos2.a -lpthread -lcomedi -lm
+#cgo CFLAGS: -std=c99
+#cgo LDFLAGS: "-L/home/kurtkl/go/src/github.com/sigvartmh/TTK4145-project/drivertest" -lpthread -lcomedi -lm
 #include "io.h"
 #include "channels.h"
 #include "elev.h"
 */
 import "C"
+
 //import "time"
 type elev_button_type_t int
 
 type elev_motor_direction_t int
 
 const (
-    BUTTON_CALL_UP elev_button_type_t = iota
-    BUTTON_CALL_DOWN
-    BUTTON_COMMAND
+	BUTTON_CALL_UP elev_button_type_t = iota
+	BUTTON_CALL_DOWN
+	BUTTON_COMMAND
 )
 
 const (
-    DIRN_DOWN elev_motor_direction_t = -1
-    DIRN_STOP = 0
-    DIRN_UP = 1
+	DIRN_DOWN elev_motor_direction_t = -1
+	DIRN_STOP                        = 0
+	DIRN_UP                          = 1
 )
 
-elevatorStatus := make(map[string]string)
+// elevatorStatus := make(map[string]string)
 
 // Eller struct?
-type elevatorStatus struct {
-    Current floor 1
+//type elevatorStatus struct {
+//    Current floor 1
 
-}
+//}
 
 func Init() int {
-    initOK := int(C.elev_init())
-    for GetFloorSensor() != 0 {
-    	setMotorDir(DIRN_DOWN)
+	initOK := int(C.elev_init())
+	for GetFloorSensor() != 0 {
+		setMotorDir(DIRN_DOWN)
 		// Fail and report after 10 seconds
-    	// time.Sleep(3*time.Second)
-    }
+		// time.Sleep(3*time.Second)
+	}
 	setMotorDir(DIRN_STOP)
 
-    go senseElevatorStatus()
-    return initOK
+	go senseElevatorStatus()
+	return initOK
 }
 
 func setMotorDir(dir elev_motor_direction_t) { // made private
-    C.elev_set_motor_direction(C.elev_motor_direction_t(dir))
+	C.elev_set_motor_direction(C.elev_motor_direction_t(dir))
 }
 
-func getFloorSensor() int{ // made private
-    return int(C.elev_get_floor_sensor_signal())
+func getFloorSensor() int { // made private
+	return int(C.elev_get_floor_sensor_signal())
 }
 
 func getButtonSignal(button elev_button_type_t, floor int) int { // made private
-    return int(C.elev_get_button_signal(C.elev_button_type_t(button), C.int(floor)))
+	return int(C.elev_get_button_signal(C.elev_button_type_t(button), C.int(floor)))
 }
 
 func getStopSignal() int { // made private
-    return int(C.elev_get_stop_signal())
+	return int(C.elev_get_stop_signal())
 }
 
 func getObstructionSignal() int { // made private
-    return int(C.elev_get_stop_signal())
+	return int(C.elev_get_stop_signal())
 }
 
-func setFloorIndicator(floor int){ // made private
-    C.elev_set_floor_indicator(C.int(floor))
+func setFloorIndicator(floor int) { // made private
+	C.elev_set_floor_indicator(C.int(floor))
 }
 
-func setButtonLamp(button elev_button_type_t, floor int, value int){ // made private
-    C.elev_set_button_lamp(C.elev_button_type_t(button), C.int(floor), C.int(value))
+func setButtonLamp(button elev_button_type_t, floor int, value int) { // made private
+	C.elev_set_button_lamp(C.elev_button_type_t(button), C.int(floor), C.int(value))
 }
 
-func setStopLamp(value int){ // made private
-    C.elev_set_stop_lamp(C.int(value))
+func setStopLamp(value int) { // made private
+	C.elev_set_stop_lamp(C.int(value))
 }
 
-func setDoorOpenLamp(value int){ // made private
-    C.elev_set_door_open_lamp(C.int(value));
+func setDoorOpenLamp(value int) { // made private
+	C.elev_set_door_open_lamp(C.int(value))
 }
 
 /*
@@ -97,12 +98,11 @@ func GoToFloor(desiredFloor int) {
 }
 */
 
-func updateFloorLights(){ // Run as go routine from Init()
-    // currentFloor := GetFloorSensor()
-    // if currentFloor
-    // setFloorIndicator(currentFloor)
+func updateFloorLights() { // Run as go routine from Init()
+	// currentFloor := GetFloorSensor()
+	// if currentFloor
+	// setFloorIndicator(currentFloor)
 }
-
 
 /*
 Public functions:
@@ -128,7 +128,3 @@ elevatorButtonStop
 
 go routine som sjekker for knapper "hele tiden" og putter dem ut på en channel som et map
 */
-
-
-
-
