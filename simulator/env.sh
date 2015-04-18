1a7ddb9f932db42304e4606004256635208b9ac0
sigvartmh <sigvart.m@gmail.com>
2015-04-18 13:03:30 +0200

    Added OSX support to enviroment

--- simulator/env.sh
+++ simulator/env.sh
@@ -1,10 +1,21 @@
-export FLOORS=4
+FLOORS=4
+GOMAXPROCS=20
 libPath=$PWD
-CGO_LDFLAGS="$libPath/driver/lib/simulation_elevator.a $libPath/driver/lib/libphobos2.a -lpthread -lm"
+if [[ "$platform"  == "Linux" ]]; then
+    CGO_LDFLAGS="$CGO_LDFLAGS -lcomedi"
+    CGO_LDFLAGS="$libPath/driver/lib/simulation_elevator.a $libPath/driver/lib/libphobos2.a -lpthread -lm"
+elif [[ "$platform"  == "Darwin" ]]; then
+    CGO_LDFLAGS="$libPath/driver/lib/osx/simulation_elevator.a $libPath/driver/lib/osx/libphobos2.a -lpthread -lm"
+fi
 platform=`uname`
 echo $platform
 if [[ "$platform"  == "Linux" ]]; then
     CGO_LDFLAGS="$CGO_LDFLAGS -lcomedi"
 fi
-echo $CGO_LDFLAGS
+echo "Linker flags: $CGO_LDFLAGS"
+echo "Max numbers of floors: $FLOORS"
+echo "Max amount of OS thread used: $GOMAXPROCS"
+export GOMAXPROCS
+export FLOORS
 export CGO_LDFLAGS
+
